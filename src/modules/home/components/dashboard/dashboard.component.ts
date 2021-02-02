9db1a383cadbc4d5137a4e4f95fe70ab3f3a2716
CrgioPeca88 <crgiopeca88@gmail.com>
2021-02-01 22:51:12 -0500

    [FEATURE] css test with events in movies cover added - dashboard.component adjusted - movie cover img added

--- src/modules/home/components/dashboard/dashboard.component.ts
+++ src/modules/home/components/dashboard/dashboard.component.ts
@@ -1,13 +1,36 @@
 // Dependencies
-import { Component } from '@angular/core';
+import { Component, ViewChild, AfterViewInit, ElementRef } from '@angular/core';
+
+export interface CoverInfo {
+  urlCover: string;
+  altCover: string;
+  titleInfoCover: string;
+  crewInfoCover: string;
+  ratingInfoCover: string;
+  showInfoCover: boolean;
+}
 
 @Component({
 	selector: 'home-dashboard',
 	templateUrl: './dashboard.component.html',
 	styleUrls: ['./dashboard.component.less']
 })
-export class DashboardComponent {
-	public _title = 'Home page under construction!';
+export class DashboardComponent implements AfterViewInit {
 
-	constructor() {}
+	@ViewChild('imgMovie', { static: true }) imgMovie: ElementRef;
+  public _showInfo: boolean;
+  public _showInfo2: boolean;
+  public _moviesCover: CoverInfo[];
+
+	constructor() {
+    this._showInfo = false;
+    this._showInfo2 = false;
+    this._moviesCover = [];
+    this.imgMovie = new ElementRef(null);
+  }
+
+  ngAfterViewInit() {
+    console.log(this.imgMovie);
+  }
+
 }
