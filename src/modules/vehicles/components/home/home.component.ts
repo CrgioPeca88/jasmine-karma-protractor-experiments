b693539d393d2e3b3ea3079a01394c3125e2f834
CrgioPeca88 <crgiopeca88@gmail.com>
2021-02-15 17:14:36 -0500

    [REFACTOR] comments removed - home.component adjusted

--- src/modules/vehicles/components/home/home.component.ts
+++ src/modules/vehicles/components/home/home.component.ts
@@ -1,41 +1,16 @@
 // Dependencies
-import { Component, OnInit, OnDestroy } from '@angular/core';
-//import { Subscription } from 'rxjs';
-//import { Router, NavigationExtras } from '@angular/router';
+import { Component } from '@angular/core';
 
 // Assets
 import { ApiBackFacadeService } from '@cs-core/services/api-back-facade.service';
-//import { Vehicle } from '@vehicles/models/Vehicle.model';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.less']
 })
-export class HomeComponent implements OnInit, OnDestroy {
+export class HomeComponent {
 
-  //private _getRentCarsSubs: Subscription;
-  //public _rentCategories: Vehicle[];
-
-  constructor(
-    //private _apiBackFacadeService: ApiBackFacadeService,
-    //private router: Router
-  ) {
-    //this._getRentCarsSubs = new Subscription();
-    //this._rentCategories = [];
-  }
-
-  ngOnInit(): void {
-    /*this._getRentCarsSubs.add(
-      this._apiBackFacadeService.getVehiclesToRent().subscribe(
-        (rentCategories: Vehicle[]) => {
-          this._rentCategories = rentCategories;
-        })
-    );*/
-  }
-
-  ngOnDestroy(): void {
-    //this._getRentCarsSubs.unsubscribe();
-  }
+  constructor() {}
 
 }
